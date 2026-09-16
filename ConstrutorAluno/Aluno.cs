05050db1480c5ff404aa6dab9cb8ea08e45fafd8
PedroHenriqueGoncalves <pehenriquegs10@gmail.com>
2026-09-16 18:44:19 -0300

    contrucao aluno

--- ConstrutorAluno/Aluno.cs
+++ ConstrutorAluno/Aluno.cs
@@ -4,9 +4,43 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace ConstrutorAluno
-{
-    public class Aluno
+{   
+     public class Aluno
     {
+        private string nome;
+        private long ra;
+        private static int qtd;
+        public string Nome 
+        { 
+            get{ return nome; }
+            set{nome = value; }
+        }
+        public long Ra 
+        { 
+            get{return ra;}
+            set{ra = value;} 
+        }
         
+        public static int Qtd
+        {
+            get{return qtd;}
+            set{qtd = value;}
+        } 
+        public Aluno(string nomeAluno, long raAluno)
+            {
+                Nome = nomeAluno;
+                Ra = 1570482423000+raAluno;
+                qtd++;
+            }
+
+        public void MostrarAtributos()
+            {
+                System.Console.WriteLine($"\tRA: {Ra} Nome: {Nome}");   
+            }
+        public void MostrarQtd()
+        {
+            System.Console.WriteLine($"\t Total de Alunos: {qtd}");
+        }
+
     }
 }
--- ConstrutorAluno/Program.cs
+++ ConstrutorAluno/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConstrutorAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno("Pedro",1);
+            Aluno a2 = new Aluno("Ana",2);
+            Aluno a3 = new Aluno("Jose",3);
+            
+            a1.MostrarAtributos();
+            a2.MostrarAtributos();
+            a3.MostrarAtributos();
+
+            a3.MostrarQtd();          
+        }
+    }
+}
+
+
+
