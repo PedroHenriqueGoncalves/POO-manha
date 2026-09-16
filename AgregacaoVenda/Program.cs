3e74b637bde305ecd53ac44f71f88a76aa5f6c42
PedroHenriqueGoncalves <pehenriquegs10@gmail.com>
2026-09-16 18:45:03 -0300

    atts

--- AgregacaoVenda/Program.cs
+++ AgregacaoVenda/Program.cs
@@ -1,5 +1,4 @@
 ﻿using AgregacaoVenda;
-
 Vendedor vendedor = new Vendedor();
 Comprador comprador = new Comprador{Verba = 10000};
 
@@ -7,15 +6,17 @@ Produto p1 = new Produto(501,"Teclado", 100.00 );
 Produto p2 = new Produto(502,"Mouse",50.00);
 
 Venda v1 = new Venda();
+
 v1.vend = vendedor;
 v1.comp = comprador;
 
 v1.vetProd.Add(p1);
 v1.vetProd.Add(p2);
+
 vendedor.CalcularComissao(p1.Preco);
 vendedor.CalcularComissao(p2.Preco);
+
 comprador.DiminuirVerba(p1.Preco);
 comprador.DiminuirVerba(p2.Preco);
+
 comprador.MostrarAtributos();
-System.Console.WriteLine("Valores Venda 1");
-v1.MostrarAtributos();
